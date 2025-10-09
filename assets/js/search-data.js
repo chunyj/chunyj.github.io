fc091b6f6154534910d502a4fcf1b94c7896bafa
chunyj <33103362+chunyj@users.noreply.github.com>
2025-10-09 18:08:10 +0000

    Deploying to gh-pages from @ chunyj/chunyj.github.io@c969021a396749d546003efea5cde122742acb4e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "updated July 2025",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
